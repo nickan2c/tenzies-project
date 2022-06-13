d9f95636ae9046d61f24782b15d53601316ed86b
nickan2c <nickan2c@gmail.com>
2022-06-13 16:54:33 +0100

    dice randomly generated

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react"
-import Die from "./components/Die"
+import Die from "./Die"
+
 
 export default function App() {
+    const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+    
+    const diceElements = dice.map(die => <Die value={die} />)
+    
     return (
-        <main >
-          <div className="die-container">
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-            <Die value={1}/>
-          </div>
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
         </main>
     )
 }
